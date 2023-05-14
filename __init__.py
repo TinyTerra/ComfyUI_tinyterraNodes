1658c6fe574f27924855154f77686a8ec19e6de1
TinyTerra <tinyterra.tc@gmail.com>
2023-05-14 15:07:25 +0200

    added auto update function (false by default)

--- __init__.py
+++ __init__.py
@@ -1,3 +1,39 @@
+# CONFIG #
+import json
+import subprocess
+from pathlib import Path
+
+cwd_path = Path(__file__).parent
+config_path = cwd_path / "config.json"
+
+# Creates a config file if it does not exist
+if not config_path.is_file():
+    with open(config_path, "w") as f:
+        json.dump({
+            "autoUpdate": False,
+        }, f, indent=4)
+
+# Open config file
+with open(config_path, "r") as f:
+    config = json.load(f)  
+
+    # Check if autoUpdate is set
+    if "autoUpdate" not in config:
+        config["autoUpdate"] = False
+
+    if config["autoUpdate"] == True:
+        try:
+            with subprocess.Popen(["git", "pull"], cwd=cwd_path, stdout=subprocess.PIPE) as p:
+                p.wait()
+                result = p.communicate()[0].decode()
+                if result == "Already up to date.\n":
+                    pass
+                else:
+                    print("\033[92m[t ttNodes Updated t]\033[0m")
+        except:
+            config["autoUpdate"] = False
+
+# MAPPING #
 from .tinyterraNodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS']
