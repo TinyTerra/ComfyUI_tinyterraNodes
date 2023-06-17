11e2cf6af49ae92a813c7825169dea131cfa0c98
TinyTerra <tinyterra.tc@gmail.com>
2023-06-17 12:09:58 +0200

    improved custom dropdown

--- js/ttN.js
+++ js/ttN.js
@@ -33,3 +33,136 @@ app.registerExtension({
 		}
 	},
 });
+
+// ttN Dropdown
+var styleElement = document.createElement("style");
+const cssCode = `
+.ttN-dropdown {
+    position: absolute;
+    box-sizing: border-box;
+    background-color: #121212;
+    border-radius: 7px;
+    box-shadow: 0 2px 4px rgba(255, 255, 255, .25);
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    z-index: 1000;
+    overflow: auto;
+    max-height: fit-content;
+}
+  
+.ttN-dropdown li {
+    padding: 4px 10px;
+    cursor: pointer;
+    font-family: system-ui;
+    font-size: 0.7rem;
+}
+  
+.ttN-dropdown li:hover,
+.ttN-dropdown li.selected {
+    background-color: #e5e5e5;
+    border-radius: 7px;
+}
+`
+styleElement.innerHTML = cssCode
+document.head.appendChild(styleElement);
+
+let activeDropdown = null;
+
+function createDropdown(inputEl, suggestions, onSelect) {
+    if (activeDropdown) {
+        activeDropdown.remove();
+        activeDropdown = null;
+    }
+
+    const dropdown = document.createElement('ul');
+    dropdown.setAttribute('role', 'listbox');
+    dropdown.classList.add('ttN-dropdown');
+
+    let selectedIndex = -1;
+
+    suggestions.forEach((suggestion, index) => {
+        const listItem = document.createElement('li');
+        listItem.setAttribute('role', 'option');
+        listItem.textContent = suggestion;
+        listItem.addEventListener('mouseover', function () {
+            selectedIndex = index;
+            updateSelection();
+        });
+        listItem.addEventListener('mouseout', function () {
+            selectedIndex = -1;
+            updateSelection();
+        });
+        listItem.addEventListener('mousedown', function (event) {
+            event.preventDefault();
+            onSelect(suggestion);
+            dropdown.remove();
+        });
+        dropdown.appendChild(listItem);
+    });
+
+    const inputRect = inputEl.getBoundingClientRect();
+    dropdown.style.top = (inputRect.top + inputRect.height) + 'px';
+    dropdown.style.left = inputRect.left + 'px';
+    dropdown.style.width = inputRect.width + 'px';
+
+    document.body.appendChild(dropdown);
+    activeDropdown = dropdown;
+
+    function updateSelection() {
+        Array.from(dropdown.children).forEach((li, index) => {
+            if (index === selectedIndex) {
+                li.classList.add('selected');
+            } else {
+                li.classList.remove('selected');
+            }
+        });
+    }
+
+    inputEl.addEventListener('keydown', function (event) {
+        const enterKeyCode = 13;
+        const escKeyCode = 27;
+        const arrowUpKeyCode = 38;
+        const arrowDownKeyCode = 40;
+        const arrowRightKeyCode = 39;
+        const arrowLeftKeyCode = 37;
+
+        if (event.keyCode === arrowUpKeyCode) {
+            event.preventDefault();
+            selectedIndex = Math.max(0, selectedIndex - 1);
+            updateSelection();
+        } else if (event.keyCode === arrowDownKeyCode) {
+            event.preventDefault();
+            selectedIndex = Math.min(suggestions.length - 1, selectedIndex + 1);
+            updateSelection();
+        } else if (event.keyCode === arrowLeftKeyCode) {
+            event.preventDefault();
+            selectedIndex = 0;  // Go to the first item
+            updateSelection();
+        } else if (event.keyCode === arrowRightKeyCode) {
+            event.preventDefault();
+            selectedIndex = suggestions.length - 1;  // Go to the last item
+            updateSelection();
+        } else if (event.keyCode === enterKeyCode && selectedIndex >= 0) {
+            event.preventDefault();
+            onSelect(suggestions[selectedIndex]);
+            dropdown.remove();
+        } else if (event.keyCode === escKeyCode) {
+            dropdown.remove();
+        }
+    });
+
+    dropdown.addEventListener('wheel', function (event) {
+        // Update dropdown.style.top by +/- 10px based on scroll direction
+        const top = parseInt(dropdown.style.top);
+        dropdown.style.top = (top + (event.deltaY < 0 ? -10 : 10)) + "px";
+    });
+
+    document.addEventListener('click', function (event) {
+        if (!dropdown.contains(event.target)) {
+            dropdown.remove();
+        }
+    });
+}
+
+export {createDropdown};
--- js/ttNembedAC.js
+++ js/ttNembedAC.js
@@ -1,65 +1,25 @@
 import { app } from "/scripts/app.js";
-
-var styleElement = document.createElement("style");
-const cssCode = `
-.autocomplete-dropdown {
-    position: absolute;
-    box-sizing: border-box;
-    background-color: #121212;
-    border-radius: 7px;
-    box-shadow: 0 2px 4px rgba(255, 255, 255, .25);
-    padding: 0;
-    margin: 0;
-    list-style: none;
-    z-index: 1000;
-    overflow: auto;
-    max-height: 200px;
-}
-  
-.autocomplete-dropdown li {
-    padding: 4px 10px;
-    cursor: pointer;
-    font-family: system-ui;
-    font-size: 0.7rem;
-}
-  
-.autocomplete-dropdown li:hover,
-.autocomplete-dropdown li.selected {
-    background-color: #e5e5e5;
-    border-radius: 7px;
-}
-`
-styleElement.innerHTML = cssCode
-document.head.appendChild(styleElement);
-
-let embeddingsList = [];
+import { createDropdown } from "./ttN.js";
 
 fetch('extensions/tinyterraNodes/embeddingsList.json')
     .then(response => response.json())
     .then(data => {
-        // Use the JSON data as a constant
-        embeddingsList = data
-            .map(embedding => "embedding:" + embedding); // Add "embedding:" to each element;
+        embeddingsList = data.map(embedding => "embedding:" + embedding);
     })
     .catch(error => {
         console.error('Error:', error);
     });
 
+let embeddingsList = [];
 
 app.registerExtension({
     name: "comfy.ttN.embeddingAC",
     nodeCreated(node) {
         if (node.widgets) {
-            // Locate dynamic prompt text widgets
-            // Include any widgets with dynamicPrompts set to true, and customtext
             const widgets = node.widgets.filter(
                 (n) => (n.type === "customtext" && n.dynamicPrompts !== false) || n.dynamicPrompts
             );
             for (const w of widgets) {
-                let autocompleteActive = false;
-                let selectedSuggestionIndex = -1;
-                const autocompleteId = 'autocomplete-dropdown';
-
                 const onInput = function () {
                     const inputText = w.inputEl.value;
                     const cursorPosition = w.inputEl.selectionStart;
@@ -75,141 +35,16 @@ app.registerExtension({
                     if (suggestionkey.startsWith(currentSegmentLower) && currentSegmentLower.length > 2 || currentSegmentLower.startsWith(suggestionkey)) {
                         const filteredEmbeddingsList = embeddingsList.filter(s => s.toLowerCase().includes(currentSegmentLower));
                         if (filteredEmbeddingsList.length > 0) {
-                            displayEmbeddingsList(filteredEmbeddingsList);
-                            autocompleteActive = true;
-                        } else {
-                            hideEmbeddingsList();
-                            autocompleteActive = false;
+                            createDropdown(w.inputEl, filteredEmbeddingsList, (selectedSuggestion) => {
+                                const newText = replaceLastEmbeddingSegment(w.inputEl.value, selectedSuggestion);
+                                w.inputEl.value = newText;
+                            });
                         }
-                    } else {
-                        hideEmbeddingsList();
-                        autocompleteActive = false;
                     }
                 };
 
                 w.inputEl.removeEventListener('input', onInput);
                 w.inputEl.addEventListener('input', onInput);
-
-
-
-                const onKeyDown = function (event) {
-                    const tabKeyCode = 9;
-                    const enterKeyCode = 13;
-                    const escKeyCode = 27;
-                    const arrowUpKeyCode = 38;
-                    const arrowDownKeyCode = 40;
-
-                    if (event.keyCode === tabKeyCode && autocompleteActive) {
-                        event.preventDefault();
-                        if (selectedSuggestionIndex !== -1) {
-                            const selectedSuggestion = document.getElementById('autocomplete-item-' + selectedSuggestionIndex);
-                            const newText = replaceLastEmbeddingSegment(w.inputEl.value, selectedSuggestion.textContent);
-                            w.inputEl.value = newText;
-                        }
-                        hideEmbeddingsList();
-                        shouldRemoveDropdown = false;
-                    } else if (event.keyCode === enterKeyCode && autocompleteActive) {
-                        event.preventDefault();
-                        if (selectedSuggestionIndex !== -1) {
-                            const selectedSuggestion = document.getElementById('autocomplete-item-' + selectedSuggestionIndex);
-                            const newText = replaceLastEmbeddingSegment(w.inputEl.value, selectedSuggestion.textContent);
-                            w.inputEl.value = newText;
-                        }
-                        hideEmbeddingsList();
-                        shouldRemoveDropdown = false;
-                    } else if (event.keyCode === arrowUpKeyCode && autocompleteActive) {
-                        event.preventDefault();
-                        if (selectedSuggestionIndex > 0) {
-                            selectedSuggestionIndex--;
-                            highlightSuggestion(selectedSuggestionIndex);
-                        }
-                    } else if (event.keyCode === arrowDownKeyCode && autocompleteActive) {
-                        event.preventDefault();
-                        if (selectedSuggestionIndex < embeddingsList.length - 1) {
-                            selectedSuggestionIndex++;
-                            highlightSuggestion(selectedSuggestionIndex);
-                        }
-                    } else if (event.keyCode === escKeyCode && autocompleteActive) {
-                        event.preventDefault();
-                        hideEmbeddingsList();
-                    }
-                };
-
-                w.inputEl.removeEventListener('keydown', onKeyDown);
-                w.inputEl.addEventListener('keydown', onKeyDown);
-
-                let shouldRemoveDropdown = false;
-                let autocompleteDropdown = null;
-
-                function displayEmbeddingsList(filteredEmbeddingsList) {
-                    hideEmbeddingsList();
-
-                    const dropdown = document.createElement('ul');
-                    dropdown.setAttribute('id', autocompleteId);
-                    dropdown.setAttribute('role', 'listbox');
-                    dropdown.classList.add('autocomplete-dropdown');
-
-                    filteredEmbeddingsList.forEach((suggestion, index) => {
-                        const listItem = document.createElement('li');
-                        listItem.setAttribute('id', 'autocomplete-item-' + index);
-                        listItem.setAttribute('role', 'option');
-                        listItem.textContent = suggestion;
-                        listItem.addEventListener('mouseover', function () {
-                            highlightSuggestion(index);
-                        });
-                        listItem.addEventListener('mousedown', function (event) {
-                            event.preventDefault();
-                            const newText = replaceLastEmbeddingSegment(w.inputEl.value, suggestion);
-                            w.inputEl.value = newText;
-                            hideEmbeddingsList();
-                            shouldRemoveDropdown = false;
-                        });
-                        dropdown.appendChild(listItem);
-                    });
-
-                    const inputRect = w.inputEl.getBoundingClientRect();
-                    dropdown.style.top = (inputRect.top + inputRect.height) + 'px';
-                    dropdown.style.left = inputRect.left + 'px';
-                    dropdown.style.width = inputRect.width + 'px';
-
-                    document.body.appendChild(dropdown);
-
-                    autocompleteDropdown = dropdown;
-                    shouldRemoveDropdown = true;
-                }
-
-                document.addEventListener('click', function (event) {
-                    if (shouldRemoveDropdown && autocompleteDropdown && !autocompleteDropdown.contains(event.target)) {
-                        hideEmbeddingsList();
-                        shouldRemoveDropdown = false;
-                        autocompleteDropdown = null;
-                    }
-                });
-
-                function hideEmbeddingsList() {
-                    const dropdown = document.getElementById(autocompleteId);
-                    if (dropdown) {
-                        dropdown.remove();
-                    }
-                    selectedSuggestionIndex = -1;
-                }
-
-                let previousSuggestionIndex = -1;
-
-                function highlightSuggestion(index) {
-                    const selectedSuggestion = document.getElementById('autocomplete-item-' + index);
-                    const previousSelectedSuggestion = document.getElementById('autocomplete-item-' + previousSuggestionIndex);
-
-                    if (previousSelectedSuggestion) {
-                        previousSelectedSuggestion.classList.remove('selected');
-                    }
-
-                    if (selectedSuggestion) {
-                        selectedSuggestion.classList.add('selected');
-                        previousSuggestionIndex = index;
-                        selectedSuggestionIndex = index;
-                    }
-                }
 
                 function replaceLastEmbeddingSegment(inputText, selectedSuggestion) {
                     const cursorPosition = w.inputEl.selectionStart;
@@ -223,7 +58,6 @@ app.registerExtension({
 
                     return inputSegments.join(' ');
                 }
-
             }
         }
     }
