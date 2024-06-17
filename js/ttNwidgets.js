7c46a4d5a818ce43f87f64d9f425537e7fd66603
TinyTerra <TinyTerra.tc@gmail.com>
2024-06-17 21:53:45 +1000

    adjust textDisplay widget handling

--- js/ttNwidgets.js
+++ js/ttNwidgets.js
@@ -5,7 +5,6 @@ class SeedControl {
     constructor(node) {
         this.node = node;
 
-        this.node.properties = this.node.properties || {};
         for (const [i, w] of this.node.widgets.entries()) {
             if (w.name === "seed" || w.name === "noise_seed") {
                 this.seedWidget = w;
@@ -34,7 +33,7 @@ function addTextDisplay(nodeType) {
     const onNodeCreated = nodeType.prototype.onNodeCreated;
     nodeType.prototype.onNodeCreated = function () {
         const r = onNodeCreated?.apply(this, arguments);
-        const w = ComfyWidgets["STRING"](this, "text", ["STRING", { multiline: true, placeholder: " " }], app).widget;
+        const w = ComfyWidgets["STRING"](this, "display", ["STRING", { multiline: true, placeholder: " " }], app).widget;
         w.inputEl.readOnly = true;
         w.inputEl.style.opacity = 0.7;
         w.inputEl.style.cursor = "auto";
@@ -46,7 +45,7 @@ function addTextDisplay(nodeType) {
         onExecuted?.apply(this, arguments);
 
         for (const widget of this.widgets) {
-            if (widget.type === "customtext"){
+            if (widget.type === "customtext" && widget.name === "display" && widget.inputEl.readOnly === true) {
                 widget.value = message.text.join('');
             }
         }
